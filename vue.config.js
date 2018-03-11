3c8e68f92423f21d946afdbb656c37b97e911dab
Mibs Balsante <mibsmyrtr@gmail.com>
2018-03-10 23:31:05 -0300

    create global scss file to import variables

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,18 @@
+const path = require('path')
+
 module.exports = {
   lintOnSave: true,
   devServer: {
     host: '0.0.0.0',
     port: 9000,
     hotOnly: true
+  },
+  css: {
+    loaderOptions: {
+      sass: {
+        includePaths: [ path.resolve('./src/assets') ],
+        data: `@import "~${path.resolve('./src/assets/app')}";`
+      }
+    }
   }
 }

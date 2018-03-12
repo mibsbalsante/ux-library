f7d7454d589da4820a92ad984214997f5c4d9290
Mibs Balsante <mibsmyrtr@gmail.com>
2018-03-12 00:01:08 -0300

    use format 'path/file.ext' to describe components in unit tests

--- tests/unit/views/Results.spec.js
+++ tests/unit/views/Results.spec.js
@@ -2,7 +2,7 @@ import { expect } from 'chai'
 import { shallow } from '@vue/test-utils'
 import Results from '@/views/Results.vue'
 
-describe('Results.vue', () => {
+describe('views/Results.vue', () => {
   it('renders props.search when passed', () => {
     const search = 'text'
     const wrapper = shallow(Results, {

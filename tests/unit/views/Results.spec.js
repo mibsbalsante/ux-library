bd577ea465ca45c60e716a9fb62dc59aed24c7cb
Mibs Balsante <mibsmyrtr@gmail.com>
2018-03-11 23:56:49 -0300

    add views/results unit test

---
+++ tests/unit/views/Results.spec.js
@@ -0,0 +1,13 @@
+import { expect } from 'chai'
+import { shallow } from '@vue/test-utils'
+import Results from '@/views/Results.vue'
+
+describe('Results.vue', () => {
+  it('renders props.search when passed', () => {
+    const search = 'text'
+    const wrapper = shallow(Results, {
+      propsData: { search }
+    })
+    expect(wrapper.props().search).to.equal(search)
+  })
+})

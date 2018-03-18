6d07a483d3fcaf8c345f5fe711bd1638260cfc6f
Mibs Balsante <mibsmyrtr@gmail.com>
2018-03-18 00:06:35 -0300

    show posts data without css

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,3 +1,24 @@
 <template>
-  <div></div>
+  <a :href="details.meta.url" class="post">
+    <div class="post_upvotes">
+      <button>up</button>
+      <div class="post_upvotes_amount">{{ details.upvotes }}</div>
+    </div>
+    <div class="post_font">{{ details.meta.url }}</div>
+    <div class="post_title">{{ details.meta.title }}</div>
+    <div class="post_info">
+      <span :class="`post_info_category label_${details.category}`">{{ details.category }}</span>
+      <span>{{ details.meta.author }}</span>
+      <span>{{ details.created_at }}</span>
+      <span>{{ details.comments }}</span>
+    </div>
+  </a>
 </template>
+
+<script>
+export default {
+  props: {
+    details: Object
+  }
+}
+</script>

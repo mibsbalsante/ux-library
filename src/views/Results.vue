8b1f65004e344c5b6f9f0621eb7179e8e1f4c84d
Mibs Balsante <mibsmyrtr@gmail.com>
2018-03-18 15:29:16 -0300

    filter results with route value, show message if no results are returned

--- src/views/Results.vue
+++ src/views/Results.vue
@@ -1,6 +1,11 @@
 <template>
   <main>
-    <post v-for="details in apiResults.links" :key="details.meta.title" :details="details"></post>
+    <p v-if="filteredResults.length === 0" class="empty">Nothing found</p>
+    <post v-else
+          v-for="details in filteredResults"
+          :key="details.meta.title"
+          :details="details">
+    </post>
   </main>
 </template>
 
@@ -18,6 +23,19 @@ export default {
     }
   },
   computed: {
+    filteredResults () {
+      if (!this.apiResults) return []
+      if (!this.search) return this.apiResults
+
+      const regex = new RegExp(this.search, 'i')
+
+      return this.apiResults.filter(post => {
+        return regex.test(post.category) ||
+                regex.test(post.meta.author) ||
+                regex.test(post.meta.title) ||
+                regex.test(post.meta.url)
+      })
+    },
     textToSearch () {
       return this.inputText ? this.inputText : this.search
     }
@@ -26,7 +44,7 @@ export default {
     getResultsFromApi () {
       fetch(apiLink)
         .then(dt => dt.json())
-        .then(json => { this.apiResults = json })
+        .then(json => { this.apiResults = json.links })
     }
   },
   components: { Post },
